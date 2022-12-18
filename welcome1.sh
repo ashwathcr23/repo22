2649ea22b08db6128010d6a6ea39b6f48193dc0c
ashwathcr <ashwathcr@ibm.com>
2022-12-18 06:26:14 +0000

    new 2nd commit

--- welcome1.sh
+++ welcome1.sh
@@ -1,2 +1,4 @@
 echo "this is the devops traning class"
 this will show the quoted text
+my name is ashwath
+i am regularly attending the classes
---
+++ welcome2.sh
@@ -0,0 +1,3 @@
+this is the new file i have craeted with vim command
+echo " welcome2.sh file created through vim command"
+
