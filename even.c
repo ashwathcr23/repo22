5f1b3b3341d4128745cabd5e94ed45e2635cff32
ashwathcr <ashwathcr@ibm.com>
2022-12-18 07:43:36 +0000

    branch2 files are 3rd commit

---
+++ even.c
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "hello"
+echo "i am working"
+hostname
+dh -h
+$0
+
---
+++ prime.c
@@ -0,0 +1,8 @@
+#!/bin bash
+echo "what is your name"
+read name
+echo "how are you, $name?"
+read remarks
+echo "i am $remarks too!"
+$o
+
