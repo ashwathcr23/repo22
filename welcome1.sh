316014b8a961ee1a9e36cd5154ca318b15caa40d
ashwathcr <ashwathcr@ibm.com>
2022-12-18 06:36:14 +0000

    merging conflict file

--- welcome1.sh
+++ welcome1.sh
@@ -1,4 +1,6 @@
 echo "this is the devops traning class"
 this will show the quoted text
 my name is ashwath
-i am regularly attending the classes
+i am regularly attending the classe
+one more line is added
+s
